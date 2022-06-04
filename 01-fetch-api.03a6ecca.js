65b675e08e25f39f08db2efee872cf75c14c7d33
NataliiaHrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-06-04 13:50:57 +0000

    Deploying to gh-pages from @ NataliiaHrynchuk/module-10@4d6fe63f9bca1caa9c396e5678f12a5270e4b43d 🚀

---
+++ 01-fetch-api.03a6ecca.js
@@ -0,0 +1,2 @@
+fetch("https://pokeapi.co/api/v2/pokemon/2/").then((o=>o.json())).then((o=>{console.log(o)})).catch((o=>{console.log(o)})),fetch("https://pokeapi.co/api/v2/pokemon");
+//# sourceMappingURL=01-fetch-api.03a6ecca.js.map
---
+++ 01-fetch-api.d6537a6c.js
@@ -0,0 +1,2 @@
+fetch("https://pokeapi.co/api/v2/pokemon/2/").then((function(o){return o.json()})).then((function(o){console.log(o)})).catch((function(o){console.log(o)})),fetch("https://pokeapi.co/api/v2/pokemon");
+//# sourceMappingURL=01-fetch-api.d6537a6c.js.map
