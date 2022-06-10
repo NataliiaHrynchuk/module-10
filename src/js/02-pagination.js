796e97b0807be59689b4e568ca6363b927d0e3f2
Nataliia Hrynchuk <natagrinchukk@gmail.com>
2022-06-10 10:57:26 +0300

    update

--- src/js/02-pagination.js
+++ src/js/02-pagination.js
@@ -12,75 +12,69 @@
  * http://newsapi.org/v2/everything?q=cat&language=en&pageSize=5&page=1
  */
 
-// import articlesTpl from './templates/articles.hbs';
+import articlesTpl from '../templates/artcles.hbs';
 import '../css/styles.css';
 import NewsApiService from '../js/news-service';
-// import LoadMoreBtn from './js/components/load-more-btn';
+import LoadMoreBtn from '../js/components/load-more-btn';
 
 const refs = {
   searchForm: document.querySelector('.js-search-form'),
   articlesContainer: document.querySelector('.js-articles-container'),
-  loadMoreBtn: document.querySelector('[data-action="load-more"]')
+  // loadMoreBtn: document.querySelector('[data-action="load-more"]')//*-тепер цього рефа не треба, бо є
+                                                                    //*-- клас LoadMoreBtn
 };
 
-// const loadMoreBtn = new LoadMoreBtn({
-//   selector: '[data-action="load-more"]',
-//   hidden: true,
-// });
+const loadMoreBtn = new LoadMoreBtn({
+  selector: '[data-action="load-more"]',
+  hidden: true,
+});
+console.log(loadMoreBtn);
+
+
 const newsApiService = new NewsApiService();
 console.log(newsApiService);
 
 refs.searchForm.addEventListener('submit', onSearch);
-refs.loadMoreBtn.addEventListener('click', onLoadMore);
-// loadMoreBtn.refs.button.addEventListener('click', fetchArticles);
+// refs.loadMoreBtn.addEventListener('click', onLoadMore);
+loadMoreBtn.refs.button.addEventListener('click', onLoadMore);
 
 function onSearch(e) {
   e.preventDefault();
 
+  
   newsApiService.query = e.currentTarget.elements.query.value;
+  if (newsApiService.query === '') {
+    return alert('Введи щось нормальне в поле запиту');
+  }
+
+  loadMoreBtn.show();//*--робимо видимою кнопку "Показати ще"
+  loadMoreBtn.disable();
   newsApiService.resetPage();
+  clearArticlesContainer();
 
   // newsApiService.fetchArticles(searchQuery);
-   newsApiService.fetchArticles();//*--вже не передаємо searchQuery, бо викор-є this.query
-
-    
+  //  newsApiService.fetchArticles().then(articles => console.log(articles));//*--вже не передаємо searchQuery, бо викор-є this.query
+  onLoadMore();
+   
 }
+
 
 function onLoadMore() {
-  // newsApiService.fetchArticles(searchQuery);
-
-   newsApiService.fetchArticles();//*--вже не передаємо searchQuery, бо викор-є this.query
+   loadMoreBtn.disable();
+  newsApiService.fetchArticles().then(articles => {
+    appendArticlesMarkup(articles);
+    loadMoreBtn.enable();
+  });
 }
-// function onSearch(e) {
-//   e.preventDefault();
+ 
 
-//   newsApiService.query = e.currentTarget.elements.query.value;
+function appendArticlesMarkup(articles) {
+  refs.articlesContainer.insertAdjacentHTML('beforeend', articlesTpl(articles));
+}
 
-//   if (newsApiService.query === '') {
-//     return alert('Введи что-то нормальное');
-//   }
-
-//   loadMoreBtn.show();
-//   newsApiService.resetPage();
-//   clearArticlesContainer();
-//   fetchArticles();
-// }
-
-// function fetchArticles() {
-//   loadMoreBtn.disable();
-//   newsApiService.fetchArticles().then(articles => {
-//     appendArticlesMarkup(articles);
-//     loadMoreBtn.enable();
-//   });
-// }
-
-// function appendArticlesMarkup(articles) {
-//   refs.articlesContainer.insertAdjacentHTML('beforeend', articlesTpl(articles));
-// }
-
-// function clearArticlesContainer() {
-//   refs.articlesContainer.innerHTML = '';
-// }
+function clearArticlesContainer() {
+  refs.articlesContainer.innerHTML = '';
+}
 
 
 
--- src/js/news-service.js
+++ src/js/news-service.js
@@ -1,25 +1,32 @@
+const API_KEY = 'b91142d7163a48d3a24979e3bb660e88';
+const BASE_URL = 'https://newsapi.org/v2';
+const options = {
+    headers: {
+        Authorization: API_KEY,
+    },
+};
+
 export default class NewsApiService {
     constructor() {
         this.searchQuery = '';
         this.page = 1;
-     }
-    
-    fetchArticles(searchQuery) {
-        console.log(this);
-         const options = {
-    headers: {
-        Authorization: 'b91142d7163a48d3a24979e3bb660e88',
-    },
-        };
-        const url = `https://newsapi.org/v2/everything?q=${this.searchQuery}&language=en&pageSize=5&page=${this.page}`;
-
-  fetch(url, options)
-    .then(r => r.json())
-      // .then(console.log())
-      .then(data => {
-          this.page += 1;
-    });
     }
+    
+    fetchArticles() {
+        console.log(this);
+           
+        const url = `${BASE_URL}/everything?q=${this.searchQuery}&language=en&pageSize=5&page=${this.page}`;
+
+        return fetch(url, options)
+           .then(response => response.json())
+        // .then(console.log())
+        .then(({articles}) => {
+            // console.log(data);
+            this.page += 1;
+
+            return articles;
+        });
+    };
 
     resetPage() {
         this.page = 1;
@@ -27,10 +34,10 @@ export default class NewsApiService {
 
     get query() {
     return this.searchQuery;
-}
+    }
 
-set query(newQuery) {
+    set query(newQuery) {
     this.searchQuery = newQuery;
-}
+    }
 }
 
